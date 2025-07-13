ece3914fcba10341c2316575e1362b63b523ac65
Seyi Ayo <oluwaseyiayoola97@gmail.com>
2025-07-13 23:44:49 +0100

    cors origin handled pending actual frontend

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,14 +4,16 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
+
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-
+        \Illuminate\Http\Middleware\HandleCors::class,
     ];
 
     protected $middlewareGroups = [
         'api' => [
+            \Illuminate\Http\Middleware\HandleCors::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
